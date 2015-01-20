be0de3e973092de8d77f94a745f7d6c6ebddc34a
Pier-Jean Lacharité <pjlacharite@MacBook-Pro-de-Pier-Jean.local>
2015-01-20 09:56:51 -0500

    Added logging + cleanup

--- src/ETagServlet.java
+++ src/ETagServlet.java
@@ -1,4 +1,6 @@
 import com.google.common.base.Strings;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
@@ -11,11 +13,14 @@ import java.util.UUID;
 
 public class ETagServlet extends HttpServlet {
     private static final String SMART_COOKIE = "smartUID";
+    private static final Logger logger = Logger.getLogger(ETagServlet.class.getCanonicalName());
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.log(Level.INFO, "Remote Address: " + request.getRemoteAddr());
         Cookie[] cookies = request.getCookies();
         String smartUID = null;
         if (cookies != null){
@@ -23,31 +28,37 @@ public class ETagServlet extends HttpServlet {
             for(Cookie cookie : cookies){
                 if(SMART_COOKIE.equals(cookie.getName())){
                     smartUID = cookie.getValue();
+                    logger.log(Level.INFO, "Cookie: " + smartUID);
                 }
             }
         }
         //Get the previous eTag
         String eTag  =  request.getHeader("If-None-Match");
+        logger.log(Level.INFO, "Previous eTag: " + eTag);
         // If no smartUID was found in cookie.
         if (!Strings.isNullOrEmpty(smartUID)){
             eTag =  request.getHeader("If-None-Match");
             if (smartUID.equals(eTag )){
                 //eTag matched, send 304 Not Modified
+                logger.log(Level.INFO, "304 Not Modified");
                 response.sendError(HttpServletResponse.SC_NOT_MODIFIED);
             }else{
                 response.setDateHeader("Last-Modified", Calendar.getInstance().getTime().getTime());
             }
         }else{
             //No cookie, check if we have a eTag.
-            if (Strings.isNullOrEmpty(eTag )){
+            if (Strings.isNullOrEmpty(eTag)){
                 //If we don't, generate a new UUID.
                 eTag  = UUID.randomUUID().toString();
+                logger.log(Level.INFO, "Generating new UUID");
             }
             //Set the cookie in the response.
             smartUID = eTag;
             response.addCookie(new Cookie(SMART_COOKIE, smartUID));
+            logger.log(Level.INFO, "Creating new cookie.");
         }
         //Update the eTag.
+        logger.log(Level.INFO, "Updating the eTag");
         response.setHeader("ETag", smartUID);
     }
 }
